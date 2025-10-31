70064ee7dce29f27921c26b0d96d83c6ff9251c4
TheUnknown550 <mattcosh06@gmail.com>
2025-10-31 17:13:33 +0700

    Fixing Error and bugs

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -10,27 +10,25 @@ def draw_plot():
     plt.figure(figsize=(10, 6))
     plt.scatter(df["Year"], df["CSIRO Adjusted Sea Level"])
 
-    # 3. Create first line of best fit
-    x = df["Year"]
-    y = df["CSIRO Adjusted Sea Level"]
-    slope, intercept, r_value, p_value, std_err = linregress(x, y)
-    plt.plot(x, slope*x + intercept, color='red')
+    # 3. Create first line of best fit (1880–2050)
+    linres1 = linregress(df["Year"], df["CSIRO Adjusted Sea Level"])
+    x_predict1 = pd.Series(range(1880, 2051))
+    y_predict1 = linres1.intercept + linres1.slope * x_predict1
+    plt.plot(x_predict1, y_predict1, 'r', label='Best Fit Line (1880–2050)')
 
-
-    # 4. Create second line of best fit
-    x = df["Year"]
-    y = df["CSIRO Adjusted Sea Level"]
-    x2 = df[df["Year"] >= 2000]["Year"]
-    y2 = df[df["Year"] >= 2000]["CSIRO Adjusted Sea Level"]
-    slope2, intercept2, r_value2, p_value2, std_err2 = linregress(x2, y2)
-    plt.plot(x2, slope2*x2 + intercept2, color='green')
+    # 4. Create second line of best fit (2000–2050)
+    df_recent = df[df["Year"] >= 2000]
+    linres2 = linregress(df_recent["Year"], df_recent["CSIRO Adjusted Sea Level"])
+    x_predict2 = pd.Series(range(2000, 2051))
+    y_predict2 = linres2.intercept + linres2.slope * x_predict2
+    plt.plot(x_predict2, y_predict2, 'green', label='Best Fit Line (2000–2050)')
 
     # 5. Add labels and title
     plt.xlabel("Year")
     plt.ylabel("Sea Level (inches)")
     plt.title("Rise in Sea Level")
-    plt.legend(["All Data", "Fit (2000)"])
+    plt.legend()
 
-    # Save plot and return data for testing (DO NOT MODIFY)
-    plt.savefig('sea_level_plot.png')
-    return plt.gca()
+    # 6. Save and return the figure
+    plt.savefig("sea_level_plot.png")
+    return plt.gcf()
